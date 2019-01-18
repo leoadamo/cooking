8ef71a9e26f2eee9450d9962aeebb22be5c3a4d5
Leonardo Adamoli <leo.adamoli@gmail.com>
2019-01-18 14:50:08 -0200

    Adicionando nova seção, atualização de estilos e testes no JS

--- js/script.js
+++ js/script.js
@@ -1,89 +1,58 @@
-var selectedRow = null;
+// Variáveis Globais
 
-/* Função enviar */
+const button_submit = document.querySelector("#enviar");
+const title = document.querySelector("input[name='title']");
+const author = document.querySelector("input[name='author']");
+const publisher = document.querySelector("input[name='publisher']");
+const data = document.querySelector("input[name='data']");
+const table_body = document.querySelector("tbody");
 
-function onFormSubmit() {
-    if (validate()) {
-        var formData = readFormData();
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
+// Objetos
+function data_submit(title, author, publisher, data) {
+    this.titulo = title;
+    this.autor = author;
+    this.editora = publisher;
+    this.data = data;
+    
+    this.newRow = function () {
+        //Criando os Elementos
+        var row = document.createElement("tr");
+        var campo_titulo = document.createElement("td");
+        var campo_autor = document.createElement("td");
+        var campo_editora = document.createElement("td");
+        var campo_data = document.createElement("td");
+        
+        //Adicionar Estilos
+        campo_titulo.className = 'table-data';
+        campo_autor.className = 'table-data';
+        campo_editora.className = 'table-data';
+        campo_data.className = 'table-data';
+        
+        //Criando Nós
+        var texto_titulo = document.createTextNode(this.titulo);
+        var texto_autor = document.createTextNode(this.autor);
+        var texto_editora = document.createTextNode(this.editora);
+        var texto_data = document.createTextNode(this.data);
+        
+        //Vinculando Nós
+        campo_titulo.appendChild(texto_titulo);
+        campo_autor.appendChild(texto_autor);
+        campo_editora.appendChild(texto_editora);
+        campo_data.appendChild(texto_data);
+        row.appendChild(campo_titulo);
+        row.appendChild(campo_autor);
+        row.appendChild(campo_editora);
+        row.appendChild(campo_data);
+        
+        //Vinculando Elementos ao Doc
+        table_body.appendChild(row);
     }
+};
+
+//Funções
+function submit (event) {
+    event.preventDefault();
 }
 
-/* Função lê form */
-
-function readFormData() {
-    var formData = {};
-    formData["title"] = document.getElementById("title").value;
-    formData["author"] = document.getElementById("author").value;
-    formData["publisher"] = document.getElementById("publisher").value;
-    formData["data"] = document.getElementById("data").value;
-    return formData;
-}
-
-/* Função insere */
-
-function insertNewRecord(data) {
-    var table = document.getElementById("list").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.title;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.author;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.publisher;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.data;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
-
-/* Apaga dados */
-
-function resetForm() {
-    document.getElementById("title").value = "";
-    document.getElementById("author").value = "";
-    document.getElementById("publisher").value = "";
-    document.getElementById("data").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("title").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("author").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("publisher").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("data").value = selectedRow.cells[3].innerHTML;
-}
-
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.title;
-    selectedRow.cells[1].innerHTML = formData.author;
-    selectedRow.cells[2].innerHTML = formData.publisher;
-    selectedRow.cells[3].innerHTML = formData.data;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("list").deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-
-function validate() {
-    isValid = true;
-    if (document.getElementById("title").value == "") {
-        isValid = false;
-        document.getElementById("ValidationError").classList.remove("hide");
-    } else {
-        isValid = true;
-        if (!document.getElementById("ValidationError").classList.contains("hide"))
-            document.getElementById("ValidationError").classList.add("hide");
-    }
-    return isValid;
-}
+//Rotina
+button_submit.addEventListener('click', data_submit);
