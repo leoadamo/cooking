c2148c1e0fb8ab98e31ea29f1b134148d1b6f8ed
Leonardo Adamoli <leo.adamoli@gmail.com>
2019-06-23 21:22:12 -0300

    finalizando responsivo e aplicando scroll suave

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const navigationLinks = document.querySelectorAll('.nav-bar a[href^="#"]');
+  const internalLinks = document.querySelectorAll('.nav-bar a[href^="#"]');
 
+  internalLinks.forEach(item => {
+    item.addEventListener('click', scrollToId);
+  })
+
+  function scrollToId(event) {
+    event.preventDefault();
+    const section = getOffsetTop(event.target);
+    scrollToPosition(section);
+  }
+
+  function getOffsetTop(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop + 40;
+  }
+
+  function scrollToPosition(section) {
+    window.scroll({
+      top: section,
+      behavior: 'smooth',
+    });
+  }
 });
